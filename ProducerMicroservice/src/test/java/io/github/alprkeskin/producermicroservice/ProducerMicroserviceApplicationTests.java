f3376d166a0c7bf1ca2b899d72db1f278b5950f5
alprKeskin <99187103+alprKeskin@users.noreply.github.com>
2022-09-14 11:06:19 +0300

    created ProducerMicroservice

---
+++ ProducerMicroservice/src/test/java/io/github/alprkeskin/producermicroservice/ProducerMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.alprkeskin.producermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
