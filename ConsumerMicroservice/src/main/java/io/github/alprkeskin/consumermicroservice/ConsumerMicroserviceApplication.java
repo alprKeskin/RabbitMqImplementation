d562692e3e592c342fdde18b1a39d1f37862e2fb
alprKeskin <99187103+alprKeskin@users.noreply.github.com>
2022-09-14 12:23:25 +0300

    created ConsumerMicroservice

---
+++ ConsumerMicroservice/src/main/java/io/github/alprkeskin/consumermicroservice/ConsumerMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package io.github.alprkeskin.consumermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerMicroserviceApplication.class, args);
+    }
+
+}
---
+++ ConsumerMicroservice/src/test/java/io/github/alprkeskin/consumermicroservice/ConsumerMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.alprkeskin.consumermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
